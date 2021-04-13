17e8fcffc1c23ccac47e4b141021e66a6e747479
bilal74 <b.khalid13@gmail.com>
2021-04-13 17:34:34 +0530

    final

--- bilal.java
+++ bilal.java
@@ -1,3 +1,10 @@
 public class bilal {
-    one two three
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+    }
+
+    // git add. 
+    // git commit -m "hell"
+    // git push 
 }
